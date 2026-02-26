f9e2c01d0bed5a9845b26683090144fc81006385
jayvalsur1-svg <jayvalsur1@gmail.com>
2026-02-26 11:48:21 +0530

    Delete os assistant directory

--- os assistant/musicplaylist.py
+++
@@ -1,8 +0,0 @@
-library={
-    "myordinarylife":"https://www.youtube.com/watch?v=9Zj0JOHJR-s",
-    "iwillsurvive":"https://www.youtube.com/watch?v=ulHcNwK1q_U&list=RDMM&start_radio=1&rv=9Zj0JOHJR-s",
-    "davidkushner":"https://www.youtube.com/watch?v=73E0BnFs7S4&list=RDMM&index=15",
-    "darkred":"https://www.youtube.com/watch?v=5da3D9ZfMQo&list=RDMM&index=12",
-    "discord":"https://www.youtube.com/watch?v=08ub3-OLidA&list=RDMM&index=27",
-    "judas":"https://www.youtube.com/watch?v=tzGmZKQIZZE&list=RDMM&index=1"
-}
